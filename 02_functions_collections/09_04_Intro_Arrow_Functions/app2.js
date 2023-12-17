3fc8b3d7cf8fbc0a9daebdc7298e2a5fa0a87391
Jose Garcia <josegarcia19809@gmail.com>
2023-12-17 13:49:42 -0600

    Intro Arrow Functions

--- 02_functions_collections/09_04_Intro_Arrow_Functions/app2.js
+++ 02_functions_collections/09_04_Intro_Arrow_Functions/app2.js
@@ -16,24 +16,24 @@ console.log(square(5));
 imprimirLinea();
 
 // Parenthesis are option whe you have one parameter
-const isEven =num =>{
-    return num % 2===0;
+const isEven = num => {
+    return num % 2 === 0;
 }
 console.log(isEven(10));
 console.log(isEven(13));
 imprimirLinea();
 
 // Must include parenthesis for multiple parameters
-const multiply = (a,b)=>{
-    return a*b;
+const multiply = (a, b) => {
+    return a * b;
 }
 
-console.log(multiply(5,6));
-console.log(multiply(8,4));
+console.log(multiply(5, 6));
+console.log(multiply(8, 4));
 imprimirLinea();
 
 // Must include parenthesis for zero parameters
-const greet= ()=>{
+const greet = () => {
     console.log("HI");
 }
 
