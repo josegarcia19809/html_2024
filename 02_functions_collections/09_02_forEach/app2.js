1e9cddbd4468aab06fbc6b0d8c176c1e7fd787a3
Jose Garcia <josegarcia19809@gmail.com>
2023-12-16 19:21:51 -0600

    Foreach

---
+++ 02_functions_collections/09_02_forEach/app2.js
@@ -0,0 +1,60 @@
+const numbers = [20, 21, 22, 23, 24, 25, 26, 27];
+
+function imprimirLinea() {
+    console.log("-------------------------------------------")
+}
+
+numbers.forEach(function (num) {
+    console.log(num * 2);
+})
+imprimirLinea();
+
+function tripleN(n) {
+    console.log(n * 3);
+}
+
+numbers.forEach(tripleN);
+imprimirLinea();
+
+numbers.forEach(function (num, idx) {
+    console.log(idx, num)
+});
+imprimirLinea();
+
+const books = [{
+    title: 'Good Omens',
+    authors: ['Terry Pratchett', 'Neil Gaiman'],
+    rating: 4.25
+},
+    {
+        title: 'Bone: The Complete Edition',
+        authors: ['Jeff Smith'],
+        rating: 4.42
+    },
+    {
+        title: 'American Gods',
+        authors: ['Neil Gaiman'],
+        rating: 4.11
+    },
+    {
+        title: 'A Gentleman in Moscow',
+        authors: ['Amor Towles'],
+        rating: 4.36
+    }
+]
+
+// Imprimir solo los títulos
+books.forEach((book) => {
+    console.log(book.title.toUpperCase());
+});
+imprimirLinea();
+
+// Imprimir solo los títulos usando for...of
+for(let book of books){
+    console.log(book.title);
+}
+imprimirLinea();
+// Imprimir solo los títulos usando un ciclo for
+for (let i = 0; i < books.length; i++) {
+    console.log(books[i].title.toUpperCase());
+}
