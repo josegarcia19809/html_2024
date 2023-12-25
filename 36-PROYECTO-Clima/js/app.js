31072c0806efb73cf0497f96815ec86271922722
Jose Garcia <josegarcia19809@gmail.com>
2023-12-25 08:24:03 -0600

    API de Clima con Fetch API: Primeros pasos con la App

---
+++ 36-PROYECTO-Clima/js/app.js
@@ -0,0 +1,16 @@
+const container = document.querySelector('.container');
+const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario');
+
+window.addEventListener('load', () => {
+    formulario.addEventListener('submit', buscarClima);
+});
+
+function buscarClima(e) {
+    e.preventDefault();
+    console.log("Buscando clima...");
+    // Validar los datos
+
+    // Consultar la API
+    
+}
