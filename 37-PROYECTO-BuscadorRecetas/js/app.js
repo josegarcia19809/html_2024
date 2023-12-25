f1949085866386be11eaaaae5f5f1889ac99eddb
Jose Garcia <josegarcia19809@gmail.com>
2023-12-25 17:19:54 -0600

    App de Recetas con Fetch API y LocalStorage: Leyendo los Resultados de la consulta

--- 37-PROYECTO-BuscadorRecetas/js/app.js
+++ 37-PROYECTO-BuscadorRecetas/js/app.js
@@ -28,8 +28,28 @@ function seleccionarCategoria(e) {
     const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`
     fetch(url)
         .then(respuesta => respuesta.json())
-        .then(resultado => console.log(resultado));
+        .then(resultado => mostrarRecetas(resultado.meals));
+}
 
+function mostrarRecetas(recetas = []) {
+    recetas.forEach(receta => {
+        const {idMeal, strMeal, strMealThumb} = receta;
+        const recetaContenedor = document.createElement("DIV");
+        recetaContenedor.classList.add('col-md-4');
+
+        const recetaCard = document.createElement("DIV");
+        recetaCard.classList.add("card", "mb-4");
+
+        const recetaImagen = document.createElement("IMG");
+        recetaImagen.classList.add("card-img-top");
+        recetaImagen.alt = `Imagen de la receta ${strMeal}`;
+        recetaImagen.src = strMealThumb;
+
+        const recetaCardBody=document.createElement("DIV");
+        recetaCardBody.classList.add("card-body");
+        
+        console.log(recetaImagen);
+    })
 }
 
 document.addEventListener("DOMContentLoaded", iniciarApp);
