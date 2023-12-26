0da5527ed595d81a0ebb0b77de0c03187ee2b135
Jgarcia <josegarcia19809@gmail.com>
2023-12-26 13:04:46 -0600

    Buscador de Imagenes con Pixabay API: Mostrar los Resultados de la consulta

--- 38-PROYECTO-PixabayImagenes/js/app.js
+++ 38-PROYECTO-PixabayImagenes/js/app.js
@@ -39,7 +39,7 @@ function mostrarAlerta(mensaje) {
 
 function buscarImagenes(termino) {
     const key = llave.value;
-    const url = `https://pixabay.com/api/?key=${key}&q=${termino}`;
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=100`;
     fetch(url)
         .then(respuesta => respuesta.json())
         .then(resultado => {
@@ -56,7 +56,20 @@ function mostrarImagenes(imagenes) {
     imagenes.forEach(imagen=>{
         const {previewURL, likes, views, largeImageURL}= imagen;
         resultado.innerHTML+=`
-        <a href="${largeImageURL}">Ver imagen</a>
+        <div class="w-1/2 md: w-1/3 lg: w-1/4 p-3 mb-4">
+            <div class="bg-white">
+                <img class="w-full" src="${previewURL}" />
+                <div class="p-4">
+                    <p class="font-bold">${likes}<span class="font-light"> Me gusta</span></p>
+                    <p class="font-bold">${views}<span class="font-light"> veces vista</span></p>
+                    <a
+                     class="block w-full bg-blue-800 hover:bg-blue-500 text-white
+                     uppercase font-bold text-center rounded mt-5 p-1"
+                     href="${largeImageURL}" target="_blank" rel="noopener noreferrer">
+                    Ver imagen</a>
+                </div>
+            </div>
+        </div>
         `;
     })
 }
