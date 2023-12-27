3bb9efb995cce21bdfa12ea02209c38f06d8c0a5
Jgarcia <josegarcia19809@gmail.com>
2023-12-26 20:22:41 -0600

    Cotizador de Criptomonedas: Leer la moneda y criptomoneda seleccionada

--- 39-PROYECTO-Criptomonedas/js/app.js
+++ 39-PROYECTO-Criptomonedas/js/app.js
@@ -1,9 +1,41 @@
 //leer la criptomoneda seleccionada
 const criptomonedaSelect = document.querySelector("#criptomonedas");
+const monedaSelect = document.querySelector("#moneda");
+
+const formulario = document.querySelector("#formulario");
+
+
+const objBusqueda = {
+    moneda: "",
+    criptomoneda: ""
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     consultarCriptomonedas();
+    formulario.addEventListener("submit", submitFormulario);
+    criptomonedaSelect.addEventListener("change", leerValor);
+    monedaSelect.addEventListener("change", leerValor);
 })
+
+function submitFormulario(e) {
+    e.preventDefault();
+
+    // Validar
+    const {moneda, criptomoneda} = objBusqueda;
+    if (moneda === "" || criptomoneda === "") {
+        mostrarAlerta("Ambos campos son obligatorios");
+        return;
+    }
+}
+
+function mostrarAlerta(mensaje) {
+
+
+}
+
+function leerValor(e) {
+    objBusqueda[e.target.name] = e.target.value;
+}
 
 function consultarCriptomonedas() {
     const url = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD`
